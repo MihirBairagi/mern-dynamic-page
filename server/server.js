2e60e3d08c7156fa9fbffff38e8d2751fb80762c
MihirBairagi <mihirbairagi10@gmail.com>
2026-08-12 23:01:59 +0530

    page.js import correction

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import Page from "./models/Page.js";
+import Page from "./models/page.js";
 
 dotenv.config();
 
